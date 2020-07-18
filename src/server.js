308f6e916e5c426f882e8034d018c6023faf56a3
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-19 04:04:05 +0900

    console chat app(nickname, message)

--- src/server.js
+++ src/server.js
@@ -17,8 +17,17 @@ const handleListening = () => console.log(`server running: http://localhost:${PO
 const server = app.listen(PORT,handleListening);
 const io = socketIO.listen(server); // socket 서버 변수
 
-// 서버 -> 누군가 연결 -> 이벤트 발생 -> 접속 브라우저로
-io.on("connection", (socket) => { // 방금 접속한 socket
-    //setTimeout(() => socket.broadcast.emit("Hello"),5000); // 이벤트 발생
-    socket.on("Hello guys",() => console.log("client say!"));
+// socket 통신 -> 서버 메모리 -> 쿠키 필요x
+// 새로고침(파일 다시 저장) -> nodemon이 서버 다시 시작 -> 기존 socket들 -> 연결만 유지, 메모리는 다 지워짐
+// 메모리 유지 -> 데이터베이스
+io.on("connection", (socket) => { 
+    socket.on("New Message",({ message }) => {
+        socket.broadcast.emit("messageNotif", { // socket에게 받은 message와 그 socket의 nickname 전달
+            message:message,
+            nickName: socket.nickName
+        });
+    });
+    socket.on("setNickName",({nickName}) => {
+        socket.nickName = nickName; // socket은 객체
+    })
 }); 
--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,19 @@
 const socket = io("/");
-socket.on("Hello", () => console.log("Somebody joined")); // 서버로부터의 이벤트를 듣고 있음
 
-setTimeout(() => socket.emit("Hello guys"),5000); // 클라이언트 이벤트 
+function setNickName(nickName){
+    socket.emit("setNickName", {nickName});
+
+}
+
+// 새로 연결된 socket
+function sendMessage(message){
+    socket.emit("New Message", {message:message});
+    console.log(`You : ${message}`);
+}
+
+// 기존에 연결되어있던 socket
+function handleMessageNotif(data){
+    const {message, nickName} = data;
+    console.log(`${nickName} : ${message}`);
+}
+socket.on("messageNotif", handleMessageNotif);
